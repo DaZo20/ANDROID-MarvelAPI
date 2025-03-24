ed116f0ca937d6bfd88feb3d9bd1a387342da28f
David Muñoz <david.munyoz97@gmail.com>
2025-03-24 20:44:03 +0100

    Added project dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,16 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlinx.serialization.json)
+}
+
+val localProperties = Properties().apply{
+    load(File(rootProject.projectDir, "local.properties").inputStream())
 }
 
 android {
@@ -19,12 +28,18 @@ android {
     }
 
     buildTypes {
+        debug {
+            buildConfigField("String", "PUBLIC_KEY","\"${localProperties["PUBLIC_KEY"]}\"")
+            buildConfigField("String", "PRIVATE_KEY","\"${localProperties["PRIVATE_KEY"]}\"")
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            buildConfigField("String", "PUBLIC_KEY","\"${localProperties["PUBLIC_KEY"]}\"")
+            buildConfigField("String", "PRIVATE_KEY","\"${localProperties["PRIVATE_KEY"]}\"")
         }
     }
     compileOptions {
@@ -43,12 +58,43 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    //ExtendedIcons
+    implementation(libs.icons.core)
+    implementation(libs.icons.extended)
+
+    //ConstraintLayout
+    implementation(libs.constraint.layout)
+
+    //Retrofit
+    implementation(libs.retrofit.retrofit2)
+    implementation(libs.retrofit.converter)
+
+    //OkHttp3 Logging
+    implementation(libs.okhttp.logging)
+
+    // Dagger Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    implementation(libs.hilt.navigation.compose)
+
+    //Coil
+    implementation(libs.coil.compose)
+    
+    //Navigation Compose
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
+
+    //Paging3
+    implementation(libs.paging.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,4 +102,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Mockk
+    testImplementation(libs.mockk.core)
+    androidTestImplementation(libs.mockk.android)
 }
