177a5cf07622f9d138f92f20babf1cfdc5c760b0
David Muñoz <david.munyoz97@gmail.com>
2025-03-29 09:45:57 +0100

    Added domain layer for detail feature and added search by name

--- app/src/main/java/com/dmolaya/dev/marvelapi/characters/domain/model/Character.kt
+++ app/src/main/java/com/dmolaya/dev/marvelapi/characters/domain/model/Character.kt
@@ -7,6 +7,11 @@ import com.google.gson.annotations.SerializedName
 data class Character(
     val id: Int,
     val name: String,
+    val description: String,
     val comics: Comics,
     val thumbnail: String
-)
+){
+    companion object {
+        val EMPTY = Character(0, "", "", Comics.EMPTY, "")
+    }
+}
